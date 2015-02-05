cd9ff388141ad155b74c869da3c92eaafe043fcb
Sebastien Deleuze <sdeleuze@pivotal.io>
2015-02-05 15:55:37 +0100

    Add client and server projects

---
+++ client/src/test/java/server/ClientApplicationTests.java
@@ -0,0 +1,19 @@
+package server;
+
+import client.ClientApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ClientApplication.class)
+@WebAppConfiguration
+public class ClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
