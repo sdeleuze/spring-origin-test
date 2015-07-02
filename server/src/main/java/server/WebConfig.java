6a7300ad270773d26c2a57e6a5cf721e99c5f6ee
Sebastien Deleuze <sdeleuze@pivotal.io>
2015-07-02 18:12:35 +0200

    Small fixes

--- server/src/main/java/server/WebConfig.java
+++ server/src/main/java/server/WebConfig.java
@@ -19,6 +19,7 @@ package server;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -27,6 +28,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/index.html").addResourceLocations("classpath:static/index.html");
+	}
 
 	//@Override
 	//public void addCorsMappings(CorsRegistry registry) {
